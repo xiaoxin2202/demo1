fef19d413a7044b0928b45ad00081d289ba1ffbc
xiaoxin2202 <huytam2202@gmail.com>
2022-05-05 22:44:12 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/controller/HelloWordl.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWordl {
+}
